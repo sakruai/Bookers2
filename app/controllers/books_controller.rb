3e7ba9809f0016cf53a435f3a9871ad793fffdb5
EC2 Default User <ec2-user@ip-172-31-8-118.ap-northeast-1.compute.internal>
2023-05-29 14:44:39 +0000

    Bookers2

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,57 @@
+class BooksController < ApplicationController
+  def new
+    @book = Book.new
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      flash[:notice] = "successfully."
+     redirect_to book_path(@book)
+    else
+      @user = current_user
+      @books = Book.all
+     render :index
+    end 
+  end
+  
+  def index
+    @books = Book.all
+    @book = Book.new
+    @user = current_user
+  end
+  
+  def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "successfully."
+    redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+  end
+  
+  def show
+    @book = Book.find(params[:id])
+    @book_new = Book.new
+    @user = @book.user
+  end
+  
+   def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+  
+  # 投稿データのストロングパラメータ
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body,)
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  
+  devise_for :users
+  root to: "homes#top"
+  
+  resources :books, only: [:new, :index, :show, :edit, :update, :create, :destroy]
+  resources :users, only: [:index, :show, :edit, :update]
+  
+  get '/home/about' => 'homes#about', as: 'about'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
